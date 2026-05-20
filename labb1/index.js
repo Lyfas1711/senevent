1c3a2e0c4dfc1a89cd010be56163c8376575937a
SALIF THIONGANE <salifthiongane17@gmail.com>
2026-05-20 20:49:11 +0000

    refactore le starter en fonctions flechees

--- labb1/index.js
+++ labb1/index.js
@@ -1,22 +1,21 @@
-"use strict ";
-( function () {
-    window.addEventListener (" load ", init ) ;
+"use strict";
 
-    function init () {
-        id ("b1").addEventListener (" click ", change ) ;
-    }
+(function () {
+    // 1. Les helpers d'abord (fonctions fléchées ultra-courtes en une seule ligne)
+    const id = name => document.getElementById(name);
+    const qs = selector => document.querySelector(selector);
 
-    function change () {
-        qs ("p").style.color = "red ";
-        qs ("h2").style.color = " blue ";
-    }
+    // 2. La fonction qui modifie les couleurs
+    const change = () => {
+        qs("p").style.color = "red";
+        qs("h2").style.color = "blue";
+    };
 
-    function id ( name ) {
-        return document.getElementById ( name ) ;
-    }
+    // 3. La fonction d'initialisation qui utilise "id" et "change"
+    const init = () => {
+        id("b1").addEventListener("click", change);
+    };
 
-    function qs ( selector ) {
-        return document.querySelector ( selector ) ;
-    }
-
-}) () ;
+    // 4. L'écouteur global qui utilise "init" tout en bas
+    window.addEventListener("load", init);
+})();
